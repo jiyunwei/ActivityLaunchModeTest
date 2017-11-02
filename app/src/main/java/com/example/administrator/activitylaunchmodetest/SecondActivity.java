b26c86c49aad20a20a5c0f815a537f9e0f0147c4
jiyunwei <375292577@qq.com>
2017-11-02 13:32:32 +0800

    initial commit

---
+++ app/src/main/java/com/example/administrator/activitylaunchmodetest/SecondActivity.java
@@ -0,0 +1,43 @@
+package com.example.administrator.activitylaunchmodetest;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+public class SecondActivity extends AppCompatActivity implements View.OnClickListener {
+    private Button first,second,main;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second);
+        first= (Button) findViewById(R.id.first);
+        second= (Button) findViewById(R.id.second);
+        main= (Button) findViewById(R.id.main);
+
+        first.setOnClickListener(this);
+        second.setOnClickListener(this);
+        main.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent;
+        switch (v.getId()){
+            case R.id.first:
+                intent=new Intent(this,FirstActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.second:
+                intent=new Intent(this,SecondActivity.class);
+                startActivity(intent);
+                break;
+            case R.id.main:
+                intent=new Intent(this,MainActivity.class);
+                startActivity(intent);
+                break;
+
+        }
+    }
+}
